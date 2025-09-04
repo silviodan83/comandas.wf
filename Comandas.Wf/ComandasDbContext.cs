812d7eff3a1e2539d387746850ffbcffdc09e943
Silvio Nespoli Dan <silviodan83@yahoo.com.br>
2025-09-03 21:34:52 -0300

    Criação do DBContext

---
+++ Comandas.Wf/ComandasDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Comandas.Wf.Modelos;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comandas.Wf
+{
+    public class ComandasDbContext:DbContext
+    {
+        public DbSet<Mesa>  Mesas { get; set; }
+        public DbSet<CardapioItem> CardapioItems { get; set; }
+        public DbSet<Comanda> Comandas { get; set; }
+        public DbSet<ComandaItens> ComandaItens { get; set; }
+        public DbSet<PedidoCozinha> PedidoCozinhas { get; set; }
+        public DbSet<PedidoCozinhaItem> PedidoCozinhaItems { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=ComandasDb;Trusted_Connection=True;TrustServerCertificate=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Comandas.Wf/Migrations/20250903235837_versao1.cs
@@ -0,0 +1,197 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Comandas.Wf.Migrations
+{
+    /// <inheritdoc />
+    public partial class versao1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CardapioItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PossuiPreparo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CardapioItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comandas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumeroMesa = table.Column<int>(type: "int", nullable: false),
+                    NomeCliente = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SituacaoComanda = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comandas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mesas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumeroMesa = table.Column<int>(type: "int", nullable: false),
+                    SituacaoMesa = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mesas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeUsuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmailUsuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SenhaUsuario = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ComandaItens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CardapioItemId = table.Column<int>(type: "int", nullable: false),
+                    ComandaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComandaItens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ComandaItens_CardapioItems_CardapioItemId",
+                        column: x => x.CardapioItemId,
+                        principalTable: "CardapioItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ComandaItens_Comandas_ComandaId",
+                        column: x => x.ComandaId,
+                        principalTable: "Comandas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PedidoCozinhas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ComandaId = table.Column<int>(type: "int", nullable: false),
+                    SituacaoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PedidoCozinhas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PedidoCozinhas_Comandas_ComandaId",
+                        column: x => x.ComandaId,
+                        principalTable: "Comandas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PedidoCozinhaItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PedidoCozinhaId = table.Column<int>(type: "int", nullable: false),
+                    ComandaItemId = table.Column<int>(type: "int", nullable: false),
+                    ComandaItensId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PedidoCozinhaItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PedidoCozinhaItems_ComandaItens_ComandaItensId",
+                        column: x => x.ComandaItensId,
+                        principalTable: "ComandaItens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PedidoCozinhaItems_PedidoCozinhas_PedidoCozinhaId",
+                        column: x => x.PedidoCozinhaId,
+                        principalTable: "PedidoCozinhas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComandaItens_CardapioItemId",
+                table: "ComandaItens",
+                column: "CardapioItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComandaItens_ComandaId",
+                table: "ComandaItens",
+                column: "ComandaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidoCozinhaItems_ComandaItensId",
+                table: "PedidoCozinhaItems",
+                column: "ComandaItensId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidoCozinhaItems_PedidoCozinhaId",
+                table: "PedidoCozinhaItems",
+                column: "PedidoCozinhaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidoCozinhas_ComandaId",
+                table: "PedidoCozinhas",
+                column: "ComandaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Mesas");
+
+            migrationBuilder.DropTable(
+                name: "PedidoCozinhaItems");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "ComandaItens");
+
+            migrationBuilder.DropTable(
+                name: "PedidoCozinhas");
+
+            migrationBuilder.DropTable(
+                name: "CardapioItems");
+
+            migrationBuilder.DropTable(
+                name: "Comandas");
+        }
+    }
+}
