833f5269937228cf01c32774e68a865c5686ee14
Silvio Nespoli Dan <silviodan83@yahoo.com.br>
2025-09-24 21:09:34 -0300

    Populando o banco de dados

--- Comandas.Wf/InicializarBanco.cs
+++ Comandas.Wf/InicializarBanco.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Comandas.Wf.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,57 @@ namespace Comandas.Wf
                     SenhaUsuario = "123"
                 });
             }
+            if (!comandasDbContext.Mesas.Any())
+            {
+                comandasDbContext.Mesas.Add(new Modelos.Mesa
+                {
+                    NumeroMesa = 1,
+                    SituacaoMesa = 0
+                    
+                });
+                comandasDbContext.Mesas.Add(new Modelos.Mesa
+                {
+                    NumeroMesa = 2,
+                    SituacaoMesa = 0
+                });
+                comandasDbContext.Mesas.Add(new Modelos.Mesa
+                {
+                    NumeroMesa = 3,
+                    SituacaoMesa = 0,
+                });
+            }
+            if (!comandasDbContext.CardapioItems.Any())
+            {
+                comandasDbContext.CardapioItems.Add(new CardapioItem
+                {
+                    Descricao = "Coca Cola Lata 350ml",
+                    PossuiPreparo = false,
+                    Preco = 5m,
+                    Titulo = "Coca Cola Lata 350ml",
+                });
+                comandasDbContext.CardapioItems.Add(new CardapioItem
+                {
+                    Descricao = "Fanta Laranja Lata 350ml",
+                    PossuiPreparo = false,
+                    Preco = 5m,
+                    Titulo = "Fanta Laranja Lata 350ml",
+                });
+                comandasDbContext.CardapioItems.Add(new CardapioItem
+                {
+                    Descricao = "Bland de 180g, pão de brioche, salada e bacon",
+                    PossuiPreparo = true,
+                    Preco = 25m,
+                    Titulo = "X-Bacon"
+                });
+                comandasDbContext.CardapioItems.Add(new CardapioItem
+                {
+                    Descricao = "Bland de 180g, pão de brioche e salada",
+                    PossuiPreparo = true,
+                    Preco = 20m,
+                    Titulo = "X-Salada"
+                });
+            }
+
             // Salva as alterações
             comandasDbContext.SaveChanges();
         }
